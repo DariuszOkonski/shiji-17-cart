b0b87219694c05893f8701be42d92a4155fdf0d9
Dariusz Okoński <darek200180@gmail.com>
2024-04-22 12:08:37 +0200

    add increase

--- src/reducer.js
+++ src/reducer.js
@@ -21,6 +21,19 @@ const reducer = (state, action) => {
     };
   }
 
+  if (action.type === INCREASE) {
+    const newCart = new Map(state.cart);
+    const itemId = action.payload.id;
+    const item = newCart.get(itemId);
+    const newItem = { ...item, amount: item.amount + 1 };
+    newCart.set(itemId, newItem);
+
+    return {
+      ...state,
+      cart: newCart,
+    };
+  }
+
   throw new Error(`No matching action type: ${action.type}`);
 };
 
